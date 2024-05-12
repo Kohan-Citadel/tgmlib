d2e8427e7db4dde5e6331e6c01e67c5faaba043b
Sceadu <44327463+sceadu37@users.noreply.github.com>
2024-05-12 15:50:21 -0400

    Adding company and index remapper

--- tgmlib.py
+++ tgmlib.py
@@ -6,6 +6,56 @@ from pathlib import Path
 from dataclasses import dataclass, field
 from pprint import pprint
 
+comp_mods_lookup = {
+	0x24: 'RESUPPLY_RATE_BONUS',
+	0x25: 'ZONE_OF_CONTROL_BONUS',
+	0x26: 'MORALE_BONUS',
+	0x27: 'MORALE_LOSS_RATE_BONUS',
+	0x28: 'MORALE_RECOVERY_RATE_BONUS',
+	0x29: 'MORALE_CHECK_BONUS',
+	0x2A: 'VISUAL_RANGE_BONUS',
+	0x2B: 'ENTRENCHMENT_RATE_BONUS',
+}
+
+unit_mods_lookup = {
+	0x01: 'ATTACK_BONUS_TO_ANY',
+	0x02: 'ATTACK_BONUS_TO_MOUNTED',
+	0x03: 'HIT_POINTS_BONUS',
+	0x04: 'ATTACK_BONUS_TO_SHADOW',
+	0x05: 'ATTACK_BONUS_TO_BUILDING',
+	0x06: 'ATTACK_BONUS_TO_ARCHER',
+	0x07: 'ATTACK_BONUS_TO_ROUTED',
+	0x08: 'DEFENSE_BONUS_VS_ANY',
+	0x09: 'DEFENSE_BONUS_VS_MOUNTED',
+	0x0A: 'UNKNOWN',
+	0x0B: 'DEFENSE_BONUS_VS_SHADOW',
+	0x0C: 'DEFENSE_BONUS_VS_ARCHER',
+	0x0D: 'WAS_DAMAGE_BONUS_TO_SHADOW',
+	0x0E: 'DAMAGE_BONUS_TO_ANY',
+	0x0F: 'WAS_DAMAGE_BONUS_TO_MOUNTED',
+	0x10: 'IGNORE_TERRAIN_BONUS',
+	0x11: 'ATTACK_BONUS_TO_NONSHADOW',
+	0x12: 'RELOAD_TIME_BONUS',
+	0x13: 'DAMAGE_TAKEN_FROM_MAGIC',
+	0x14: 'DAMAGE_TAKEN_FROM_NON_MAGIC',
+	0x15: 'DAMAGE_TAKEN_FROM_RANGED',
+	0x16: 'DAMAGE_TAKEN_FROM_MELEE',
+	0x17: 'DAMAGE_TAKEN_FROM_ANY',
+	0x18: 'REVERSE_DAMAGE_WHEN_HIT',
+	0x19: 'MOVEMENT_BONUS',
+	0x1A: 'PARALYZE_BONUS',
+	0x1B: 'ENTANGLE_BONUS',
+	0x1C: 'DAMAGE_TAKEN_FROM_HOLY',
+	0x1D: 'DAMAGE_TAKEN_FROM_UNHOLY',
+	0x1E: 'DAMAGE_TAKEN_FROM_KHALDUNITE',
+	0x1F: 'IMMUNITY_TO_ENCHANTMENT',
+	0x20: 'MELEE_HOLY_DAMAGE',
+	0x21: 'MELEE_UNHOLY_DAMAGE',
+	0x22: 'CAUSE_KHALDUNITE_DAMAGE',
+	0x23: 'CAUSE_MAGIC_DAMAGE',
+
+}
+
 
 class tgmFile:
     """
@@ -124,7 +174,8 @@ class tgmFile:
                 (self.unknown0,
                  self.num_objs,) = struct.unpack('HH', in_fh.read(4))
                 
-                self.objs = {}
+                self.by_name = {}
+                self.by_index = {}
                 for i in range(0, self.num_objs):
                     new_obj = {}
                     name = struct.unpack('32s', in_fh.read(32))[0].rstrip(b'\x00').decode('ascii')
@@ -135,8 +186,8 @@ class tgmFile:
                      new_obj['type'],) = struct.unpack('BB', in_fh.read(2))
                     
                     # This allows for lookup by either name or index, and both will point to the same mutable object
-                    self.objs[name] = new_obj
-                    self.objs[i] = new_obj
+                    self.by_name[name] = new_obj
+                    self.by_index[i] = new_obj
             return
     
     class PlrsChunk:
@@ -242,7 +293,7 @@ class tgmFile:
                      self.booty_value,) = struct.unpack('=12sBfHHfffffcf4sf', in_fh.read(54))
                     
                     #print(f'uk0name: {self.name}, player: {self.player}, index: {self.index}, id: {self.editor_id}, hs_sw: {self.hotspot_sw}, hs_se: {self.hotspot_se}')
-                    match TYPE.objs[header.index]['subtype']:
+                    match TYPE.by_index[header.index]['subtype']:
                         # Ruins
                         case 0:
                             (self.ruin_data,) = struct.unpack('=13s', in_fh.read(13))
@@ -328,7 +379,109 @@ class tgmFile:
                              self.base_mana_production,) = struct.unpack('=26sfffff', in_fh.read(46))
             
             class Company:
-                pass
+                
+                class Unit:
+                    def __init__(self, in_fh):
+                        (self.unit_index,) = struct.unpack('=B', in_fh.read(1))
+                        self.header = ObjsChunk.MapObj.ObjectHeader(in_fh)
+                        (self.flag1, self.flag2,) = struct.unpack('=', buffer)
+                        
+                def __init__(self, in_fh):
+                    (self.captain_index,
+                     self.front_index,
+                     self.support1_index,
+                     self.support2_index,
+                     self.b1,
+                     self.b2,
+                     self.b3,
+                     self.b4,
+                     self.name,) = struct.unpack('=4H4B22s', in_fh.read(34))
+                    
+                    (self.uk0,
+                     self.captain_id,
+                     self.occupy_flag0,
+                     self.control_zone,
+                     self.occupy_flag1,
+                     self.required,
+                     self.uk2,
+                     stone,
+                     wood,
+                     iron,
+                     mana,
+                     self.speed,
+                     self.f0,
+                     self.f1,
+                     self.attack_efficiency,
+                     self.formation_atk_mod,
+                     self.f4,
+                     self.experience,
+                     self.uk3,
+                     self.current_morale,
+                     self.max_morale,
+                     self.uk4,
+                     self.zone_of_control,
+                     self.uk5,
+                     u0_pos_se,
+                     u0_pos_sw,
+                     u1_pos_se,
+                     u1_pos_sw,
+                     u2_pos_se,
+                     u2_pos_sw,
+                     u3_pos_se,
+                     u3_pos_sw,
+                     u4_pos_se,
+                     u4_pos_sw,
+                     u5_pos_se,
+                     u5_pos_sw,
+                     ) = struct.unpack('=6sHIfII4s11f13s2f16sf167s12fxxxx', in_fh.read(332))
+                    
+                    self.upkeep = {
+                        'stone': stone,
+                        'wood': wood,
+                        'iron': iron,
+                        'mana': mana,
+                        }
+                    
+                    self.unit_positions = [
+                        {'se': u0_pos_se,
+                         'sw': u0_pos_sw,},
+                        {'se': u1_pos_se,
+                         'sw': u1_pos_sw,},
+                        {'se': u2_pos_se,
+                         'sw': u2_pos_sw,},
+                        {'se': u3_pos_se,
+                         'sw': u3_pos_sw,},
+                        {'se': u4_pos_se,
+                         'sw': u4_pos_sw,},
+                        {'se': u5_pos_se,
+                         'sw': u5_pos_sw,},
+                        ]
+                    
+                    (start, num_modifiers,) = struct.unpack('=II', in_fh.read(8))
+                    self.company_modifiers_provided = {}
+                    for _ in range(num_modifiers):
+                        (key, value,) = struct.unpack('=Hfx', in_fh.read(7))
+                        self.company_modifiers_provided[comp_mods_lookup[key]] = value
+                    
+                    (start, num_modifiers,) = struct.unpack('=II', in_fh.read(8))
+                    self.unit_modifiers_provided = {}
+                    for _ in range(num_modifiers):
+                        (key, value,) = struct.unpack('=Hfx', in_fh.read(7))
+                        self.unit_modifiers_provided[unit_mods_lookup[key]] = value
+                    in_fh.seek(4, 1) # skips 4byte padding
+                    
+                    self.all_company_modifiers = {}
+                    in_fh.seek(8, 1) # skips 8 'start' bytes
+                    for m in comp_mods_lookup.values():
+                        (self.all_company_modifiers[m],) = struct.unpack('=f', in_fh.read(4))
+                    
+                    (self.uk6,
+                     self.num_units,) = struct.unpack('=13sI', in_fh.read(17))
+                    
+                    
+                    
+                    
+                    
             
             # MapObj init
             def __init__(self, in_fh, TYPE):
@@ -354,10 +507,11 @@ class tgmFile:
                 self.unknown0 = struct.unpack('=4s', in_fh.read(4))
                 self.objs = []
                 
-                while in_fh.tell() < (iff.data.children[16].data_offset + iff.data.children[16].length - 18):
-                            self.objs.append(self.MapObj(in_fh, TYPE))
-                            #pprint(vars(self.objs[-1]))   
-                            #print('')
+                #while in_fh.tell() < (iff.data.children[16].data_offset + iff.data.children[16].length - 18):
+                for _ in range(6):
+                    self.objs.append(self.MapObj(in_fh, TYPE))
+                    #pprint(vars(self.objs[-1]))   
+                    #print('')
                 
                 
     
@@ -387,7 +541,7 @@ testTGM.load()
 #for obj in testTGM.OBJS.objs:
 #    print(obj)
 
-print(testTGM.OBJS.objs[30].base_iron_production)
-for child in testTGM.iff.data.children: print(f'{child.type}')
+#print(testTGM.OBJS.objs[30].base_iron_production)
+#for child in testTGM.iff.data.children: print(f'{child.type}')
 
 
--- update_map.py
+++ update_map.py
@@ -8,6 +8,7 @@ new_map = tgmlib.tgmFile(new_map_path)
 old_map.load()
 new_map.load()
 
+# Use this mapping to go from pre KG-0.9.5 (includes AG-1.3.7 and 1.3.10) to KG-0.9.6
 name_mapping = {
     'CREATURE': 'RHAKSHA',
     'DHAVRUM_BELRAAG': 'DHAVRUM_BELRAAG_CAPTAIN',
@@ -20,6 +21,9 @@ name_mapping = {
     "Garoj'mokAscended": "GAROJ'MOK_CAPTAIN",
     "Garoj'mokEnlightened": "GAROJ'MOK_CAPTAIN",
     "Garoj'mokRestored": "GAROJ'MOK_CAPTAIN",
+    'GideonAscended': 'Gideon XarrAscended',
+    'GideonEnlightened': 'Gideon XarrEnlightened',
+    'GideonRestored': 'Gideon XarrRestored',
     'KARG': 'KARG_CAPTAIN',
     'KARG corpse': 'KARG_CAPTAIN corpse',
     'KargAscended': 'KARG_CAPTAIN',
@@ -44,20 +48,38 @@ name_mapping = {
     'LUCIUS_AJAM corpse': 'LORD_JAVIDAN corpse',
     'LORD_JAVIDAN': 'DARIUS_JAVIDAN',
     'LORD_JAVIDAN corpse': 'DARIUS_JAVIDAN corpse',
+    'Lord JavidanAscended': 'Darius JavidanAscended',
+    'Lord JavidanEnlightened': 'Darius JavidanEnlightened',
+    'Lord JavidanRestored': 'Darius JavidanRestored',
     'MAUSALLAS_BAHRAM': 'MISTRESS_ROXANNA',
     'MAUSALLAS_BAHRAM corpse': 'MISTRESS_ROXANNA corpse',
     'MISTRESS_ROXANNA': 'VASHTI',
     'MISTRESS_ROXANNA corpse': 'VASHTI corpse',
+    'Mistress JavidanAscended': 'VashtiAscended',
+    'Mistress JavidanEnlightened': 'VashtiEnlightened',
+    'Mistress JavidanRestored': 'VashtiRestored',
     'MISTRESS_VASHTI': 'VASHTI',
     'MISTRESS_VASHTI corpse': 'VASHTI corpse',
+    'Mistress VashtiAscended': 'VashtiAscended',
+    'Mistress VashtiEnlightened': 'VashtiEnlightened',
+    'Mistress VashtiRestored': 'VashtiRestored',
+    'Samman RavidAscended': 'Samman OsahyrAscended',
+    'Samman RavidEnlightened': 'Samman OsahyrEnlightened',
+    'Samman RavidRestored': 'Samman OsahyrRestored',
     'SEBASTIAN_ATAFEH': 'SAR_LASHKAR2',
     'SEBASTIAN_ATAFEH corpse': 'SAR_LASHKAR2 corpse',
     'SAR_LASHKAR2': 'SAR_LASHKAR',
     'SAR_LASHKAR2 corpse': 'SAR_LASHKAR corpse',
+    'Lord Sar LashkarAscended': 'Sar LashkarAscended',
+    'Lord Sar LashkarEnlightened': 'Sar LashkarEnlightened',
+    'Lord Sar LashkarRestored': 'Sar LashkarRestored',
     'SOLOMON_GHAFFAR': 'SHOHN_MAHT2',
     'SOLOMON_GHAFFAR corpse': 'SHOHN_MAHT2 corpse',
     'SHOHN_MAHT2': 'SHOHN_MAHT',
     'SHOHN_MAHT2 corpse': 'SHOHN_MAHT corpse',
+    'Lord Shohn MahtAscended': 'Shohn MahtAscended',
+    'Lord Shohn MahtEnlightened': 'Shohn MahtEnlightened',
+    'Lord Shohn MahtRestored': 'Shohn MahtRestored',
     'BANDIT_CAMP': 'BRIGAND_CAMP',
     'BANDIT_CAMP ruin': 'BRIGAND_CAMP ruin',
     'NEST': 'RHAKSHA_NEST',
@@ -66,7 +88,7 @@ name_mapping = {
     'NEW_AHRIMAN_CITADEL ruin': 'AHRIMAN_CITADEL ruin',
     }
 
-id_mapping = {}
+index_mapping = {}
 
 for k, v in old_map.TYPE.by_name.items():
     if k in name_mapping:
@@ -74,30 +96,41 @@ for k, v in old_map.TYPE.by_name.items():
     else:
         name = k
         
-    new_id = new_map.TYPE.by_name[name]['index']
-    old_id = v['index']
-    id_mapping[old_id] = new_id
+    new_index = new_map.TYPE.by_name[name]['index']
+    old_index = v['index']
+    index_mapping[old_index] = new_index
     
+for obj in old_map.OBJS.objs:
+    print(f'obj id:{obj.header.editor_id} ix:{obj.header.index} -> {index_mapping[obj.header.index]}')
+    obj.header.index = index_mapping[obj.header.index]
+    match obj.header.obj_class:
+        case 0x24:
+            # Building specific stuff goes here
+            pass
+        case 0x3C:
+            # company specific stuff goes here
+            #company speed
+            #company modifiers provided
+            #unit modifiers provided
+            #all company modifiers
+            #unit current speed
+            #unit modifiers gained
+            #unit base speed
+            #unit max hp
+            #unit mana
+            pass
+
     
     
     
 # Items to update:
     #main index and all references
         #copy correct index (TYPE) from correct blank map
-        #create mapping between old index & new
         #index locations are FTRS, OBJS, and individual units
         #include list of overrides for ini swaps
         #apply overrides to HROS
         #add adtl heroes to HROS
-    #company speed
-    #company modifiers provided
-    #unit modifiers provided
-    #all company modifiers
-    #unit current speed
-    #unit modifiers gained
-    #unit base speed
-    #unit max hp
-    #unit mana
+    
 
 #Adtl TODO
     #Add HROS chunk
