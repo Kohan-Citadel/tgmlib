383a5feb2d01adfae99bee77aaf616c36ee11dc2
Sceadu <44327463+sceadu37@users.noreply.github.com>
2024-05-29 23:54:42 -0400

    added 90deg rotations to tile symmetries

--- mirror_map.py
+++ mirror_map.py
@@ -4,71 +4,38 @@ from copy import deepcopy
 import math
 
 tile_symmetries = {
-    0x0: {'n/s': {'rotation': 0x0, 'reflection': 0x0,},
-          'e/w': {'rotation': 0x0, 'reflection': 0x0,},
-          'ne/sw': {'rotation': 0x0, 'reflection': 0x0,},
-          'nw/se': {'rotation': 0x0, 'reflection': 0x0,},},
-    0x1: {'n/s': {'rotation': 0x5, 'reflection': 0x1,},
-          'e/w': {'rotation': 0x5, 'reflection': 0x5,},
-          'ne/sw': {'rotation': 0x5, 'reflection': 0x3,},
-          'nw/se': {'rotation': 0x5, 'reflection': 0x7,},},
-    0x2: {'n/s': {'rotation': 0x6, 'reflection': 0x8,},
-          'e/w': {'rotation': 0x6, 'reflection': 0x4,},
-          'ne/sw': {'rotation': 0x6, 'reflection': 0x2,},
-          'nw/se': {'rotation': 0x6, 'reflection': 0x6,},},
-    0x3: {'n/s': {'rotation': 0x7, 'reflection': 0x7,},
-          'e/w': {'rotation': 0x7, 'reflection': 0x3,},
-          'ne/sw': {'rotation': 0x7, 'reflection': 0x1,},
-          'nw/se': {'rotation': 0x7, 'reflection': 0x5,},},
-    0x4: {'n/s': {'rotation': 0x8, 'reflection': 0x6,},
-          'e/w': {'rotation': 0x8, 'reflection': 0x2,},
-          'ne/sw': {'rotation': 0x8, 'reflection': 0x8,},
-          'nw/se': {'rotation': 0x8, 'reflection': 0x4,},},
-    0x5: {'n/s': {'rotation': 0x1, 'reflection': 0x5,},
-          'e/w': {'rotation': 0x1, 'reflection': 0x1,},
-          'ne/sw': {'rotation': 0x1, 'reflection': 0x7,},
-          'nw/se': {'rotation': 0x1, 'reflection': 0x3,},},
-    0x6: {'n/s': {'rotation': 0x2, 'reflection': 0x4,},
-          'e/w': {'rotation': 0x2, 'reflection': 0x8,},
-          'ne/sw': {'rotation': 0x2, 'reflection': 0x6,},
-          'nw/se': {'rotation': 0x2, 'reflection': 0x2,},},
-    0x7: {'n/s': {'rotation': 0x3, 'reflection': 0x3,},
-          'e/w': {'rotation': 0x3, 'reflection': 0x7,},
-          'ne/sw': {'rotation': 0x3, 'reflection': 0x5,},
-          'nw/se': {'rotation': 0x3, 'reflection': 0x1,},},
-    0x8: {'n/s': {'rotation': 0x4, 'reflection': 0x2,},
-          'e/w': {'rotation': 0x4, 'reflection': 0x6,},
-          'ne/sw': {'rotation': 0x4, 'reflection': 0x4,},
-          'nw/se': {'rotation': 0x4, 'reflection': 0x8,},},
-    0x9: {'n/s': {'rotation': 0x9, 'reflection': 0x9,},
-          'e/w': {'rotation': 0x9, 'reflection': 0x9,},
-          'ne/sw': {'rotation': 0x9, 'reflection': 0xA,},
-          'nw/se': {'rotation': 0x9, 'reflection': 0xA,},},
-    0xA: {'n/s': {'rotation': 0xA, 'reflection': 0xA,},
-          'e/w': {'rotation': 0xA, 'reflection': 0xA,},
-          'ne/sw': {'rotation': 0xA, 'reflection': 0x9,},
-          'nw/se': {'rotation': 0xA, 'reflection': 0x9,},},
-    0xB: {'n/s': {'rotation': 0xC, 'reflection': 0xB,},
-          'e/w': {'rotation': 0xC, 'reflection': 0xC,},
-          'ne/sw': {'rotation': 0xC, 'reflection': 0xE,},
-          'nw/se': {'rotation': 0xC, 'reflection': 0xD,},},
-    0xC: {'n/s': {'rotation': 0xB, 'reflection': 0xC,},
-          'e/w': {'rotation': 0xB, 'reflection': 0xB,},
-          'ne/sw': {'rotation': 0xB, 'reflection': 0xD,},
-          'nw/se': {'rotation': 0xB, 'reflection': 0xE,},},
-    0xD: {'n/s': {'rotation': 0xE, 'reflection': 0xE,},
-          'e/w': {'rotation': 0xE, 'reflection': 0xD,},
-          'ne/sw': {'rotation': 0xE, 'reflection': 0xC,},
-          'nw/se': {'rotation': 0xE, 'reflection': 0xB,},},
-    0xE: {'n/s': {'rotation': 0xD, 'reflection': 0xD,},
-          'e/w': {'rotation': 0xD, 'reflection': 0xE,},
-          'ne/sw': {'rotation': 0xD, 'reflection': 0xB,},
-          'nw/se': {'rotation': 0xD, 'reflection': 0xC,},},
-    0xF: {'n/s': {'rotation': 0xF, 'reflection': 0xF,},
-          'e/w': {'rotation': 0xF, 'reflection': 0xF,},
-          'ne/sw': {'rotation': 0xF, 'reflection': 0xA,},
-          'nw/se': {'rotation': 0xF, 'reflection': 0xA,},},
-    
+    0x0: {'rotation': (0x0, 0x0, 0x0, 0x0,),
+          'reflection': {'n/s': 0x0, 'e/w': 0x0, 'ne/sw': 0x0, 'nw/se': 0x0,},},
+    0x1: {'rotation': (0x1, 0x3, 0x5, 0x7,),
+          'reflection': {'n/s': 0x1, 'e/w': 0x5, 'ne/sw': 0x3, 'nw/se': 0x7,},},
+    0x2: {'rotation': (0x2, 0x4, 0x6, 0x8,),
+          'reflection': {'n/s': 0x8, 'e/w': 0x4, 'ne/sw': 0x2, 'nw/se': 0x6,},},
+    0x3: {'rotation': (0x3, 0x5, 0x7, 0x1,),
+          'reflection': {'n/s': 0x7, 'e/w': 0x3, 'ne/sw': 0x1, 'nw/se': 0x5,},},
+    0x4: {'rotation': (0x4, 0x6, 0x8, 0x2,),
+          'reflection': {'n/s': 0x6, 'e/w': 0x2, 'ne/sw': 0x8, 'nw/se': 0x4,},},
+    0x5: {'rotation': (0x5, 0x7, 0x1, 0x3,),
+          'reflection': {'n/s': 0x5, 'e/w': 0x1, 'ne/sw': 0x7, 'nw/se': 0x3,},},
+    0x6: {'rotation': (0x6, 0x8, 0x2, 0x4,),
+          'reflection': {'n/s': 0x4, 'e/w': 0x8, 'ne/sw': 0x6, 'nw/se': 0x2,},},
+    0x7: {'rotation': (0x7, 0x1, 0x3, 0x5,),
+          'reflection': {'n/s': 0x3, 'e/w': 0x7, 'ne/sw': 0x5, 'nw/se': 0x1,},},
+    0x8: {'rotation': (0x8, 0x2, 0x4, 0x6,),
+          'reflection': {'n/s': 0x2, 'e/w': 0x6, 'ne/sw': 0x4, 'nw/se': 0x8,},},
+    0x9: {'rotation': (0x9, 0xA, 0x9, 0xA,),
+          'reflection': {'n/s': 0x9, 'e/w': 0xA, 'ne/sw': 0x9, 'nw/se': 0xA,},},
+    0xA: {'rotation': (0xA, 0x9, 0xA, 0x9,),
+          'reflection': {'n/s': 0xA, 'e/w': 0xA, 'ne/sw': 0x9, 'nw/se': 0x9,},},
+    0xB: {'rotation': (0xB, 0xE, 0xC, 0xD,),
+          'reflection': {'n/s': 0xB, 'e/w': 0xC, 'ne/sw': 0xE, 'nw/se': 0xD,},},
+    0xC: {'rotation': (0xC, 0xD, 0xB, 0xE,),
+          'reflection': {'n/s': 0xC, 'e/w': 0xB, 'ne/sw': 0xD, 'nw/se': 0xE,},},
+    0xD: {'rotation': (0xD, 0xB, 0xE, 0xC,),
+          'reflection': {'n/s': 0xE, 'e/w': 0xD, 'ne/sw': 0xB, 'nw/se': 0xC,},},
+    0xE: {'rotation': (0xE, 0xC, 0xD, 0xB,),
+          'reflection': {'n/s': 0xD, 'e/w': 0xE, 'ne/sw': 0xB, 'nw/se': 0xC,},},
+    0xF: {'rotation': (0xF, 0xA, 0xF, 0xA,),
+          'reflection': {'n/s': 0xF, 'e/w': 0xF, 'ne/sw': 0xA, 'nw/se': 0xA,},},
     }
 
 # from https://stackoverflow.com/a/3838398
@@ -202,10 +169,12 @@ def mirror(tgm: tgmlib.tgmFile, sections, source_region, **kwargs):
                     if new_tile.layout in tile_symmetries:
                         #print(f'flipping tile')
                         try:
-                            new_tile.layout = tile_symmetries[new_tile.layout][symmetry_axis][symmetry_type]
+                            new_layout = tile_symmetries[new_tile.layout][symmetry_type]
+                            new_tile.layout = new_layout[pos] if symmetry_type == 'rotation' else new_layout[symmetry_axis]
                         except Exception:
-                            #print('  missing tile rotation info')
-                            pass
+                            print('  missing tile rotation info')
+                            new_tile.terrain1 = 0xE
+                            new_tile.terrain2 = 0xE
                     else:
                         new_tile.terrain1 = 0xB
                         new_tile.terrain2 = 0xB
